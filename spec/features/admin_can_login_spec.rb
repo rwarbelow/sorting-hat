c1778e30177dfeb3ec4e472bb67dd8fbfd680a08
Erinna Chen <erinna.chen@gmail.com>
2016-02-26 16:37:38 -0700

    Admin can login to account

---
+++ spec/features/admin_can_login_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.feature "Admin login" do
+  scenario "sees admin dashboard after logging in" do
+    username = "allan"
+    password = "password"
+    create_user(username, password, 1)
+
+    visit "/"
+    click_on "Login"
+
+    fill_in "Username", with: username
+    fill_in "Password", with: password
+
+    click_on "Login"
+
+    expect(page).to have_content "Welcome allan!"
+    expect(page).to have_content "List users"
+  end
+end
