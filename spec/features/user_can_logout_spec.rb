a97e0d3417eb9dfa1fb11a105cf49a44fc979e7a
Erinna Chen <erinna.chen@gmail.com>
2016-02-28 14:26:01 -0700

    Add user can logout feature

---
+++ spec/features/user_can_logout_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.feature "User logout" do
+  scenario "sees welcome page after logout" do
+    username = "harry"
+    password = "hedwig"
+    create_user(username, password)
+    login(username, password)
+    expect(page).to have_content "Welcome harry!"
+
+    click_on "Logout"
+
+    expect(current_path).to eq root_path
+  end
+end
