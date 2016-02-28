de41ce2f6757cb5e41281011f8e34baa96655bb8
Erinna Chen <erinna.chen@gmail.com>
2016-02-28 14:47:40 -0700

    Add check for user not to be able to see personal page after logout

--- spec/features/user_can_logout_spec.rb
+++ spec/features/user_can_logout_spec.rb
@@ -13,4 +13,16 @@ RSpec.feature "User logout" do
     expect(current_path).to eq root_path
     expect(page).to have_content "You have successfully logged out"
   end
+
+  scenario "can't access personal page after logout" do
+    username = "harry"
+    password = "hedwig"
+    harry = create_user(username, password)
+    login(username, password)
+    click_on "Logout"
+
+    visit user_path(harry.id)
+
+    expect(page).to have_content("The page you were looking for doesn't exist.")
+  end
 end
