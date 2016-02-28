618ee6d6461cd7ffaf254ac645a6492fbd144972
Erinna Chen <erinna.chen@gmail.com>
2016-02-28 14:13:51 -0700

    Add not logged in user feature test

--- spec/features/user_can_see_their_points_and_rewards_spec.rb
+++ spec/features/user_can_see_their_points_and_rewards_spec.rb
@@ -13,7 +13,21 @@ RSpec.feature "User can see points and rewards" do
     expect(page).to have_content "Total points: 4000"
     within("#rewards") do
       expect(page).to have_content "Golden Snitch"
-      expect(page).to have_content "25"
     end
+  end
+
+  scenario "if not logged in, get the 404 page" do
+    username = "harry"
+    password = "hedwig"
+    harry = create_user(username, password)
+    harry.points.create(value: 4000)
+    harry.rewards.create(name: "Golden Snitch", cost: 25)
+
+    visit user_path(harry.id)
+
+    expect(page).not_to have_content "Welcome harry!"
+    expect(page).not_to have_content "Total points: 4000"
+    expect(page).not_to have_content "Golden Snitch"
+    expect(page).to have_content "The page you were looking for doesn't exist."
   end
 end
